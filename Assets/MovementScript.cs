ed9812deea84306f1c6a77e8250963eb4a42275b
stepandanilov <5digitnoob@gmail.com>
2021-04-04 12:24:07 +0900

    added moving ball as a player

---
+++ Assets/MovementScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementScript : MonoBehaviour
+{
+    public float speed = 0.03f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("w"))
+        {
+            Vector3 vector = new Vector3(0, speed, 0);
+            transform.position += vector;
+        }
+        if (Input.GetKey("a"))
+        {
+            Vector3 vector = new Vector3(-1 * speed, 0, 0);
+            transform.position += vector;
+        }
+        if (Input.GetKey("s"))
+        {
+            Vector3 vector = new Vector3(0, -1 * speed, 0);
+            transform.position += vector;
+        }
+        if (Input.GetKey("d"))
+        {
+            Vector3 vector = new Vector3(speed, 0, 0);
+            transform.position += vector;
+        }
+    }
+}
