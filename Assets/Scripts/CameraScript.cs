d1d3fd554ac373ba057cf98f90f63f1af8e9aff3
stepandanilov <5digitnoob@gmail.com>
2021-04-23 10:31:28 +0900

    added menu and smooth camera follow script

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public Transform player;
+    public float smoothSpeed = 0.125f;
+    public float offset = -10.0f;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float charPosX = player.position.x;
+        float charPosY = player.position.y;
+
+        Vector3 desiredPosition = new Vector3(charPosX, charPosY, offset);
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        transform.position = smoothedPosition;
+    }
+}
