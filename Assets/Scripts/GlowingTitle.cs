d3eb5dec70e3e2b1e0c1a73d7efd89861b495718
stepandanilov <5digitnoob@gmail.com>
2021-05-18 15:37:28 +0900

    changed main menu visuals, beginning on settings changes

---
+++ Assets/Scripts/GlowingTitle.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlowingTitle : MonoBehaviour
+{
+    public TMPro.TMP_Text text;
+    public float glowChangeSpeed = 0.01f;
+    
+    private float glowPower = 0.5f;
+    private bool goingUp = true;
+    private float max = 0.8f;
+    private float min = 0.2f;
+    // Update is called once per frame
+    void Update()
+    {
+        text.fontSharedMaterial.SetFloat("_GlowPower", glowPower);
+        if (goingUp)
+        {
+            glowPower += glowChangeSpeed;
+            if (glowPower > max) goingUp = false;
+        }
+        else
+        {
+            glowPower -= glowChangeSpeed;
+            if (glowPower < min) goingUp = true;
+        }
+    }
+}
