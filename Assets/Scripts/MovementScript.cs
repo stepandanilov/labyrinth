96bd95ac7a828be9bc4e845189a30a2b28c2a4ff
stepandanilov <5digitnoob@gmail.com>
2021-05-19 18:50:10 +0900

    changed movement, it's now smoother

--- Assets/Scripts/MovementScript.cs
+++ Assets/Scripts/MovementScript.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class MovementScript : MonoBehaviour
 {
-    private float speed = 3f;
+    private float speed = 5f;
     public Rigidbody2D rigidBody;
     // Start is called before the first frame update
     void Start()
@@ -15,9 +15,10 @@ public class MovementScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Vector2 movement = new Vector2(Input.GetAxis("Horizontal") * speed, Input.GetAxis("Vertical") * speed);
-        //rigidBody.AddForce(movement);     
-        Vector3 movement = new Vector3(Input.GetAxis("Horizontal") * speed, Input.GetAxis("Vertical") * speed, 0.0f);
-        transform.position += movement * Time.deltaTime;
+        rigidBody.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, Input.GetAxis("Vertical") * speed);
+        //rigidBody.AddForce(movement);
+        
+        //Vector3 movement = new Vector3(Input.GetAxis("Horizontal") * speed, Input.GetAxis("Vertical") * speed, 0.0f);
+        //transform.position += movement * Time.deltaTime;
     }
 }
